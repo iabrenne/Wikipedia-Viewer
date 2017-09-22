a12ca4abff37c5d0603374ba9f604f3183f61ef3
Irina Brennen <irinabrennen@twc.com>
2017-09-21 20:51:16 -0500

    Added basic wiki search functionality. Up to 10 search results will be returned. Title and extract of the wiki articles matching search criteria will be displayed on the page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 var searchRequestValueChanged = false;
 
+var url = "https://en.wikipedia.org/w/api.php?";
+
+var myArr;
+
+var queryString = {
+	"action":"query",
+	"format":"json",
+	"prop":"extracts",
+	"generator":"search",
+	"exsentences":1,
+	"exlimit":10,
+	"exintro":1,
+	"explaintext":1,
+	"gsrlimit":10,
+	"origin":"*"
+};
+
+ 
+
 $(document).ready(function(){
 
 	$("#search-box").on("change",function(){
@@ -12,8 +31,26 @@ $(document).ready(function(){
 		/* Checking if user pressed an Enter key */
 		if (myKeyCode == 13) {
 			if (searchRequestValueChanged){
-				window.alert("Searching for " + $("#search-box").val()+ "!");
-				searchRequestValueChanged = false;
+				queryString.gsrsearch = $("#search-box").val();
+
+				/* move search bar to the top to make room for search results */
+				$("#search-div").css("bottom","auto");
+				$("#search-div").css("top","1vw");
+
+				/* GET request to wiki api for the topic selected */
+				$.getJSON(url,queryString,function(data){
+					myArr = Object.keys(data.query.pages);
+				
+					var myHtml = "";
+					myArr.forEach(function (value) {
+						
+						myHtml = $("#results").html() + "<br>" + data.query.pages[value].title 
+						            + "<br>" + data.query.pages[value].extract + "<br>";	
+						$("#results").html(myHtml);				
+					});
+
+				});
+	
 			}
 
 		}
