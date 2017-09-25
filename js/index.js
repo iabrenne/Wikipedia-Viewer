0c44aafa387123e4c27b1dbb253d7b577eb18dbf
Irina Brennen <irinabrennen@twc.com>
2017-09-25 12:05:58 -0500

    Added clickable links to the page for each article

--- js/index.js
+++ js/index.js
@@ -7,14 +7,15 @@ var myArr;
 var queryString = {
 	"action":"query",
 	"format":"json",
-	"prop":"extracts",
+	"prop":"extracts|info",
 	"generator":"search",
 	"exsentences":1,
 	"exlimit":10,
 	"exintro":1,
 	"explaintext":1,
+	"inprop":"url",
 	"gsrlimit":10,
-	"origin":"*"
+	"origin":"*"	
 };
 
  
@@ -43,12 +44,15 @@ $(document).ready(function(){
 				
 					/* Go through each wiki result and place it into its own div */
 					myArr.forEach(function (value, index) {
-						
+
+						var aId = "#anchor" + (index+1);
 						var h2Div = "#res" + (index + 1) + ">h2";
 						var pDiv = "#res" + (index + 1) + ">p";
+						var link = data.query.pages[value].fullurl;
 
 						$(h2Div).html(data.query.pages[value].title);
 						$(pDiv).html(data.query.pages[value].extract);
+						$(aId).attr("href",link);
 								
 					});
 
